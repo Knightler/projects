3311564b51bed0bd632049fe5c9ddcf3d2cf9580
Knightler <aryan.haghighinew@gmail.com>
2025-11-03 11:53:33 +0300

    initial commit for the webrover and made packages universal for all projects

---
+++ WebRover/main.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv
+from langchain_openai.chat_models import ChatOpenAI
+from langchain_core.tools import tool
+from langchain_core.messages import SystemMessage, HumanMessage
+
+
