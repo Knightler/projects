9934916668805bd18f9df4010eb92b0e2be2f819
Knightler <aryan.haghighinew@gmail.com>
2025-11-03 12:22:27 +0300

    initial commit for the fastapi-tutarial project for cementing my knowledge in fastapi before starting other projects

--- WebRover/main.py
+++ WebRover/main.py
@@ -2,5 +2,16 @@ from dotenv import load_dotenv
 from langchain_openai.chat_models import ChatOpenAI
 from langchain_core.tools import tool
 from langchain_core.messages import SystemMessage, HumanMessage
+import os
+import requests
+from fastapi import FastAPI
 
+load_dotenv()
+llm = ChatOpenAI(
+        model="gpt-4o-mini"
+        )
 
+@tool
+def fetch_website(url: str) -> str:
+  response = requests.get(url)
+  return response.text
